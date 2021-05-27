850370b3407d77249e414c04cca83909cd9aa087
Mahdi Amiri Shavaki <mahdi_amiri98@comp.iust.ac.ir>
2021-05-27 22:14:52 +0430

    Top 11 tables create queries (select and drop queries are just for test)

--- Online_Service_Market.sql
+++ Online_Service_Market.sql
@@ -1,1 +1,117 @@
+create table service_provider(
+	username varchar(50) not null,
+	passwrd varchar(50) not null,
+	address_on_social_networks varchar(50),
+	phone_number varchar(15),
+	licence_number varchar(15),
+	start_year date,
+	location_range_of_service nvarchar(100),
+	primary key (username)
+);
 
+create table gallery(
+	photo image not null,
+	photo_id int identity(10000, 1),
+	username varchar(50) not null,
+	primary key (photo_id, username),
+	foreign key (username) references service_provider(username)
+);
+
+create table specialist(
+	first_name nvarchar(100),
+	last_name nvarchar(100) not null,
+	gender varchar(6),
+	age int,
+	degree_of_education varchar(100),
+	username varchar(50) not null,
+	primary key (username),
+	foreign key (username) references service_provider(username)
+);
+
+create table company(
+	website varchar(100),
+	username varchar(50) not null,
+	primary key (username),
+	foreign key (username) references service_provider(username)
+);
+
+create table company_manager(
+	first_name nvarchar(100),
+	last_name nvarchar(100) not null,
+	company_username varchar(50) not null,
+	primary key (company_username),
+	foreign key (company_username) references company(username)
+);
+
+create table client(
+	first_name nvarchar(100),
+	last_name nvarchar(100) not null,
+	phone_number varchar(15),
+	wallet_value money not null,
+	username varchar(50) not null,
+	passwrd varchar(50) not null,
+	primary key(username)
+);
+
+create table initial_order(
+	description_of_requested_work nvarchar(2000) not null,
+	estimation_price_request varchar(50) not null,
+	id int identity(10000, 1) not null,
+	primary key (id, estimation_price_request),
+	foreign key (estimation_price_request) references client(username)
+);
+
+create table estimate_price(
+	initial_order_id int not null,
+	initial_order_estimation_price_request varchar(50) not null,
+	service_provider varchar(50) not null,
+	estimated_price money not null,
+	primary key (initial_order_id, initial_order_estimation_price_request, service_provider, estimated_price),
+	foreign key (service_provider) references service_provider(username),
+	foreign key (initial_order_id, initial_order_estimation_price_request) references initial_order(id, estimation_price_request)
+);
+
+create table finalized_order(
+	client_comment nvarchar(2000),
+	rating int not null,
+	price money not null,
+	initial_order_id int not null,
+	initial_order_estimation_price_request varchar(50) not null,
+	id int identity(10000, 1),
+	primary key(initial_order_id, initial_order_estimation_price_request, id),
+	foreign key (initial_order_id, initial_order_estimation_price_request) references initial_order(id, estimation_price_request)
+);
+
+create table picks(
+	client varchar(50) not null,
+	initial_order_id int not null,
+	initial_order_estimation_price_request varchar(50) not null,
+	service_provider varchar(50) not null,
+	primary key(client, initial_order_id, initial_order_estimation_price_request, service_provider),
+	foreign key (client) references client(username),
+	foreign key (initial_order_id, initial_order_estimation_price_request) references initial_order(id, estimation_price_request),
+	foreign key (service_provider) references service_provider(username)
+);
+
+select * from service_provider;
+select * from gallery; 
+select * from specialist;
+select * from company;
+select * from company_manager;
+select * from finalized_order;
+select * from initial_order;
+select * from estimate_price;
+select * from client;
+select * from picks;
+
+
+drop table picks;
+drop table estimate_price;
+drop table finalized_order;
+drop table initial_order;
+drop table client;
+drop table gallery;
+drop table specialist;
+drop table company_manager;
+drop table company;
+drop table service_provider;
